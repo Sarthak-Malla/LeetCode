76f91872f67c3e379d2f4bac059bef59b230dcda
Sarthak-Malla <spm9513@nyu.edu>
2022-07-13 17:46:14 +0545

    Changed main.cpp for more possibilities
    Does not work for "tmmzuxt".

--- 3. Longest Substring Without Repeating Characters/main.cpp
+++ 3. Longest Substring Without Repeating Characters/main.cpp
@@ -8,21 +8,34 @@ int lengthOfLongestSubstring(string s){
     map<char, int> seen;
 
     int len = 0;
+    int longest = 0;
 
     if (s.length() == 1)
         return 1;
 
     for (int i = 0; i < s.length(); i++){
-        if (seen.find(s[i]) != seen.end() && i - seen[s[i]] > len)
-            len = i - seen[s[i]];
+        if (seen.find(s[i]) == seen.end())
+            len++;
+        else{
+            if (i - seen[s[i]] == 1 && i != 1)
+                len--;
+            else 
+                len -= seen[s[i]];
+                if (len < 0)
+                    len = 0;
+        }
+
+        if (len > longest)
+            longest = len;
+        
         seen[s[i]] = i;
     }
 
-    return len;
+    return longest;
 }
 
 int main(){
-    string s = "abacdef";
+    string s = "cxyzjmnopkilbkd";
 
     int length = lengthOfLongestSubstring(s);
 
