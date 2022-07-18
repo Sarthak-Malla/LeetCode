9c49dfc9976ace083f771c55fe979953041b2df9
Sarthak-Malla <spm9513@nyu.edu>
2022-07-19 02:14:07 +0545

    Working Code but Time Exceeded
    Hopefully the code works

--- 5. Longest Palindromic Substring/main.cpp
+++ 5. Longest Palindromic Substring/main.cpp
@@ -7,19 +7,67 @@ bool isPalindrome(string s){
     string reversed = "";
 
     for (int i = 0; i < s.length(); i++){
-        reversed += s[i];
+        reversed = s[i] + reversed;
     }
 
     return (s == reversed);
 }
 
 string longestPalindrome(string s){
+    int len = s.length();
+    vector<vector<int> > mat;
+    vector<int> inside;
+    for (int i = 0; i < len; i++){
+        for (int j = 0; j < len; j++){
+            inside.push_back(-1);
+        }
+
+        mat.push_back(inside);
+        inside.clear();
+    }
+
+    if (len == 1)
+        return s;
     
+    string current = "";
+    string mid = "";
+    string longest = "";
+
+    for (int i = 0; i < len; i++){
+        for (int j = i; j < len; j++){
+            current = s.substr(i, j - i + 1);
+            if (mat[i][j] < 0){
+                if (j - i >= 2){
+                    if (mat[i+1][j-1] < 0){
+                        mat[i+1][j-1] = isPalindrome(current.substr(1, current.length() - 2));
+                    }
+
+                    if (mat[i+1][j-1] && s[i] == s[j]){
+                        mat[i][j] = 1;
+                    } else {
+                        mat[i][j] = 0;
+                    }
+
+                } else {
+                    mat[i][j] = (s[i] == s[j]);
+                }
+            }
+            if (mat[i][j] == 0)
+                continue;
+
+            if (mat[i][j] == 1){
+                if (current.length() > longest.length())
+                    longest = current;
+            }
+        }
+    }
+
+    return longest;
 }
 
 int main(){
 
-    string s = "babad";
+    string s = "eabcb";
 
     string ans = longestPalindrome(s);   
 
