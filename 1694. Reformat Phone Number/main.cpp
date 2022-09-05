112d4b969a748341deba90fa95e2af583e498863
Sarthak-Malla <spm9513@nyu.edu>
2022-09-05 23:35:29 +0400

    Perfectly Working Code: 0ms time

--- 1694. Reformat Phone Number/main.cpp
+++ 1694. Reformat Phone Number/main.cpp
@@ -15,18 +15,37 @@ string giveNumbers(string phone_number){
     return number;
 }
 
-string getFormattedNumber(string number){
+string reformatNumber(string number){
+    int remainder = number.length() % 3;
+    string answer = "";
+    if (remainder == 0) {
+        for (int i = 0; i < number.length() - 3; i += 3){
+            answer += number.substr(i, 3) + '-';
+        }
 
+        answer += number.substr(number.length() - 3, 3);
+    } else if (remainder == 1) {
+        for (int i = 0; i < number.length() - 4; i += 3) {
+            answer += number.substr(i, 3) + '-';
+        }
+        answer += number.substr(number.length() - 4, 2) + '-' + number.substr(number.length() - 2, 2);
+    } else if (remainder == 2) {
+        for (int i = 0; i < number.length() - 2; i += 3) {
+            answer += number.substr(i, 3) + '-';
+        }
+        answer += number.substr(number.length() - 2, 2);
+    }
+
+    return answer;
 }
 
 int main(){
     string phone_number = "1-23-45 6";
     string p_number = giveNumbers(phone_number);
 
-    // string formatted_number = getFormattedNumber(p_number);
+    string formatted_number = reformatNumber(p_number);
 
-    // cout<<formatted_number<<endl;
-    cout<<p_number<<endl;
+    cout<<formatted_number<<endl;
 
     return 0;
 }
