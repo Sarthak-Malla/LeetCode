4d1dbb3396b5239261a03215bdde1e9dca34d9b9
Sarthak-Malla <spm9513@nyu.edu>
2022-09-07 22:06:08 +0400

    First Bad Version Working code

---
+++ 278. First Bad Version/main.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+bool isBadVersion(int version) {
+    return (version >= 99);
+}
+
+int firstBadVersion(int n) {
+    int left = 1, right = n;
+    int mid;
+
+    if (left == right && isBadVersion(left))
+        return left;
+
+    while (right - left > 1) {
+        mid = (right-left)/2 + left; // 4 2 3
+        if (isBadVersion(mid))
+            right = mid; // 4
+        else if (!isBadVersion(mid)) // 1 2 3 4 5 6 7 8
+            left = mid; // 2 3 
+    }
+
+    if (isBadVersion(left))
+        return left;
+    if (isBadVersion(right))
+        return right;
+    return mid;
+}
+
+int main(){
+    cout<<firstBadVersion(111)<<endl;
+
+    return 0;
+}
