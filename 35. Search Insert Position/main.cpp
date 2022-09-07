bc2c9a260a8b32e75dddc353b84d28b1bb7e9b3a
Sarthak-Malla <spm9513@nyu.edu>
2022-09-07 22:53:02 +0400

    Search Insert: Works but Slow(6%,16ms)

--- 35. Search Insert Position/main.cpp
+++ 35. Search Insert Position/main.cpp
@@ -4,56 +4,41 @@ using namespace std;
 
 int searchInsert(vector<int>& nums, int target) {
     int left = 0, right = nums.size() - 1;
-    int mid = (right)/2;
+    int mid;
     bool found = false;
 
-    if (right == left){
-        if (nums[left] == target)
+    if (left == right) {
+        if (target == nums[left]) 
             return left;
         else if (target < nums[left])
             return 0;
-        else if (target > nums[left])
+        else
             return 1;
     }
-    // 1 3 5 7
+    // 1 2 5 6 8
     while (right - left > 1) {
-        mid = (right-left)/2 + left; // 1 2
-        if (target < nums[mid]) {
+        mid = (right - left) / 2 + left; // 2
+        if (target < nums[mid])
             right = mid;
-        } else if (target > nums[mid]) {
-            left = mid; // 1 2
-        } else {
+        else if (target > nums[mid])
+            left = mid; // 2
+        else {
             found = true;
-            break;
+            return mid;
         }
     }
 
     if (target == nums[left])
         return left;
-    
     if (target == nums[right])
         return right;
-
-    if (!found) {
-        if (target < nums[left]) {
-            if (left == 0)
-                return left;
-        }
-
-        if (target > nums[right]) {
-            if (right == nums.size() - 1)
-                return right + 1;
-            return nums.size() - 1;
-        }
-
-        if (target < nums[mid]){
-            return mid;
-        } else if (target > nums[mid]) {
-            if (mid == nums.size() - 1)
-                return mid + 1;
-            return nums.size() - 1;
-        }
-    }
+    
+    if (target < nums[left])
+        return left;
+    else if (target > nums[left] && target < nums[right])
+        return right;
+    else if (target > nums[right])
+        return right+1;
 
     return mid;
 }
@@ -64,9 +49,9 @@ int main(){
     nums.push_back(2);
     nums.push_back(5);
     nums.push_back(6);
-    nums.push_back(7);
+    nums.push_back(8);
 
-    int target = 3;
+    int target = 0;
 
     cout<<searchInsert(nums, target)<<endl;
 
