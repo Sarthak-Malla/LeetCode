ed6242d4aeb74075f2be5be9276ba6c9724d1359
Sarthak-Malla <spm9513@nyu.edu>
2022-09-08 10:30:07 +0400

    Working Square Sorted Array [Time Limit]

--- 35. Search Insert Position/main.cpp
+++ 35. Search Insert Position/main.cpp
@@ -3,52 +3,52 @@
 using namespace std;
 
 int searchInsert(vector<int>& nums, int target) {
-    // int left = 0, right = nums.size() - 1;
-    // int mid;
-    // bool found = false;
+    int left = 0, right = nums.size() - 1;
+    int mid;
+    bool found = false;
 
-    // if (left == right) {
-    //     if (target == nums[left]) 
-    //         return left;
-    //     else if (target < nums[left])
-    //         return 0;
-    //     else
-    //         return 1;
-    // }
-    // // 1 2 5 6 8
-    // while (right - left > 1) {
-    //     mid = (right - left) / 2 + left; // 2
-    //     if (target < nums[mid])
-    //         right = mid;
-    //     else if (target > nums[mid])
-    //         left = mid; // 2
-    //     else {
-    //         found = true;
-    //         return mid;
-    //     }
-    // }
-
-    // if (target == nums[left])
-    //     return left;
-    // if (target == nums[right])
-    //     return right;
-    
-    // if (target < nums[left])
-    //     return left;
-    // else if (target > nums[left] && target < nums[right])
-    //     return right;
-    // else if (target > nums[right])
-    //     return right+1;
-
-    // return mid;
-
-    int len = nums.size(), i = 0;
-    for (i = 0; i < len; i++) {
-        if (nums[i] == target || nums[i] > target) {
-            return i;
+    if (left == right) {
+        if (target == nums[left]) 
+            return left;
+        else if (target < nums[left])
+            return 0;
+        else
+            return 1;
+    }
+    // 1 2 5 6 8
+    while (right - left > 1) {
+        mid = (right - left) / 2 + left; // 2
+        if (target < nums[mid])
+            right = mid;
+        else if (target > nums[mid])
+            left = mid; // 2
+        else {
+            found = true;
+            return mid;
         }
     }
-    return i;
+
+    if (target == nums[left])
+        return left;
+    if (target == nums[right])
+        return right;
+    
+    if (target < nums[left])
+        return left;
+    else if (target > nums[left] && target < nums[right])
+        return right;
+    else if (target > nums[right])
+        return right+1;
+
+    return mid;
+
+    // int len = nums.size(), i = 0;
+    // for (i = 0; i < len; i++) {
+    //     if (nums[i] == target || nums[i] > target) {
+    //         return i;
+    //     }
+    // }
+    // return i;
 }
 
 int main(){
--- 977. Squares of a Sorted Array/main.cpp
+++ 977. Squares of a Sorted Array/main.cpp
@@ -6,13 +6,14 @@ using namespace std;
 vector<int> sortedSquares(vector<int>& nums) {
     vector<int> s;
     int sq;
-    for (int i = 0; i < nums.size(); i++) {
-        sq = pow(nums[i], 2);
+    for (int i = 0; i < nums.size(); i++) { // 0
+        sq = pow(nums[i], 2); // 100
         if (i == 0) {
-            s.push_back(sq);
+            s.push_back(sq); // 100
         }
-        for (int j = 0; j < s.size(); j++) {
-            if (s[j] > sq || s[j] == sq) {
+        int s_size = s.size(); // 1
+        for (int j = 0; j < s_size; j++) { // 0
+            if (i != 0 && (s[j] > sq || sq == s[j])) {
                 s.insert(s.begin() + j, sq);
                 break;
             } else if (sq > s[s.size() - 1]) {
@@ -26,7 +27,7 @@ vector<int> sortedSquares(vector<int>& nums) {
 
 int main(){
     vector<int> nums;
-    nums.push_back(-10);
+    // nums.push_back(-10);
     nums.push_back(-1);
     nums.push_back(0);
     nums.push_back(3);
