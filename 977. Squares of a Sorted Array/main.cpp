5c19d25c7bb8b01b082ea46f6155da6c57293fa3
Sarthak-Malla <spm9513@nyu.edu>
2022-09-09 15:28:27 +0400

    977 Faster with Lower Space (Starting Middle)

--- 977. Squares of a Sorted Array/main.cpp
+++ 977. Squares of a Sorted Array/main.cpp
@@ -4,38 +4,30 @@
 using namespace std;
 
 vector<int> sortedSquares(vector<int>& nums) {
-    vector<int> left;
-    vector<int> right;
     vector<int> ans;
-    for (int i = 0; i < nums.size(); i++) {
-        if (nums[i] < 0)
-            left.insert(left.begin(), nums[i]);
-        else 
-            right.push_back(nums[i]);
-    }
+    int i = 0;
+    while (i < nums.size() && nums[i] < 0)
+        i++;
 
-    int l = 0, r = 0, a = 0;
-    while (l < left.size() && r < right.size()) {
-        if (pow(left[l], 2) > pow(right[r], 2) ) {
-            ans.push_back(pow(right[r], 2)); // 0 9 
-            a++; // 1 2
-            r++; // 1 2
+    int left = i-1, right = i;
+
+    while (left >= 0 && right < nums.size()) {
+        if (pow(nums[left], 2) >= pow(nums[right], 2)) {
+            ans.push_back(pow(nums[right], 2));
+            right++;
         } else {
-            ans.push_back(pow(left[l], 2));
-            a++;
-            l++;
+            ans.push_back(pow(nums[left], 2));
+            left--;
         }
     }
 
-    while (l < left.size()) {
-        ans.push_back(pow(left[l], 2));
-        a++;
-        l++;
+    while (left >= 0) {
+        ans.push_back(pow(nums[left], 2));
+        left--;
     }
-    while (r < right.size()) {
-        ans.push_back(pow(right[r], 2));
-        a++;
-        r++;
+    while (right < nums.size()) {
+        ans.push_back(pow(nums[right], 2));
+        right++;
     }
 
     return ans;
@@ -44,10 +36,13 @@ vector<int> sortedSquares(vector<int>& nums) {
 int main(){
     vector<int> nums;
     nums.push_back(-10);
-    nums.push_back(-1);
-    nums.push_back(0);
-    nums.push_back(3);
-    nums.push_back(10);
+    nums.push_back(-9);
+    // nums.push_back(0);
+    // nums.push_back(3);
+    // nums.push_back(10);
+    // nums.push_back(10);
+    // nums.push_back(10);
+    // nums.push_back(11);
 
     vector<int> ans = sortedSquares(nums);
     for (int i = 0; i < ans.size(); i++) {
