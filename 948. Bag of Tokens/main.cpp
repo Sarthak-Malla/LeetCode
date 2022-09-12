46a98d4099ee7f67787e23dd16f464673b4c93a6
Sarthak-Malla <spm9513@nyu.edu>
2022-09-12 22:34:33 +0400

    Wrong Code

---
+++ 948. Bag of Tokens/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int bagOfTokensScore(vector<int>& tokens, int power) {
+    teromuji
+}
+
+int main(){
+    vector<int> tokens;
+    tokens.push_back(71);
+    tokens.push_back(55);
+    tokens.push_back(82);
+    // tokens.push_back(100);
+    // tokens.push_back(200);
+    // tokens.push_back(300);
+    // tokens.push_back(400);
+
+    int power = 50;
+
+    cout<<bagOfTokensScore(tokens, power)<<endl;
+
+    return 0;
+}
