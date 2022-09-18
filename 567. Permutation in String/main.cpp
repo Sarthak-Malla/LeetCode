7300715c0de781654fac67d45b9dba8b65a7d558
Sarthak-Malla <spm9513@nyu.edu>
2022-09-18 23:49:48 +0400

    567: Hash Map Implementation

--- 567. Permutation in String/main.cpp
+++ 567. Permutation in String/main.cpp
@@ -1,23 +1,36 @@
 #include<iostream>
 #include<map>
 #include<iterator>
+#include<vector>
 using namespace std;
 typedef map<char,int>::iterator iterate;
 
-bool isIn(string s1, string s2) {
-    sort(s1.begin(), s1.end());
-    sort(s2.begin(), s2.end());
-
-    return s1 == s2;
+bool isIn(string s1, string s2, vector<int> &hash) {
+    vector<int> hash2(26, 0);
+    for (int i = 0; i < s2.size(); i++) {
+        hash2[s2[i] - 'a']++;
+    }
+    
+    for (int i = 0; i < 26; i++) {
+        if (hash[i] != hash2[i])
+            return false;
+    }
+    
+    return true;
 }
 
 bool checkInclusion(string s1, string s2) {
     int left = 0, n = s1.length();
     if (n > s2.length())
         return false;
+    
+    vector<int> hash(26, 0);
+    for (int i = 0; i < s1.size(); i++) {
+        hash[s1[i] - 'a']++;
+    }
 
     while (left < s2.length() - n + 1) {
-        if (isIn(s1, s2.substr(left, n)))
+        if (isIn(s1, s2.substr(left, n), hash))
             return true;
 
         left++;
@@ -28,7 +41,7 @@ bool checkInclusion(string s1, string s2) {
 
 int main(){
     string s1 = "ab";
-    string s2 = "ab";
+    string s2 = "abcde";
 
     cout<<boolalpha<<checkInclusion(s1, s2)<<endl;
 
