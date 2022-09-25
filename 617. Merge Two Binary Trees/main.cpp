4d873737ae01093185d4e39c4a546d756c4a2038
Sarthak-Malla <spm9513@nyu.edu>
2022-09-25 18:11:39 +0400

    Made TreeNode struct

--- 617. Merge Two Binary Trees/main.cpp
+++ 617. Merge Two Binary Trees/main.cpp
@@ -1,17 +1,13 @@
 #include<iostream>
 using namespace std;
-
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 
 TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
     if (root1 == nullptr && root2 == nullptr)
