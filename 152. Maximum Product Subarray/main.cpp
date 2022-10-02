e5419a94ce471df9c949b2ca0b48fe82bc4ea9ae
Sarthak-Malla <spm9513@nyu.edu>
2022-10-02 11:58:39 +0400

    152: Working Code, DP

--- 152. Maximum Product Subarray/main.cpp
+++ 152. Maximum Product Subarray/main.cpp
@@ -3,25 +3,22 @@
 using namespace std;
 
 int maxProduct(vector<int>& nums) {
-    vector<int> sums(nums.size(), 0);
-    int left = 0;
-    int max = INT_MIN;
-    while (left < nums.size()) {
-        for (int i = 0; i < sums.size(); i++) {
-            if (i == left)
-                sums[i] = nums[i];
-            
-            if (i - 1 >= left)
-                sums[i] = sums[i-1] * nums[i];
-            
-            if (sums[i] > max)
-                max = sums[i];
-        }
+    int maxNeg = nums[0];
+    int maxPos = nums[0];
+    int ans = nums[0];
+    for (int i = 1; i < nums.size(); i++) {
+        if (nums[i] < 0)
+            swap(maxPos, maxNeg);
+        
+        maxPos = max(maxPos * nums[i], nums[i]);
+        maxNeg = min(maxNeg * nums[i], nums[i]);
 
-        left++;
+        ans = max(ans, maxPos);
     }
-    return max;
+
+    return ans;
 }
+
 
 int main(){
     vector<int> nums = {2,3,-2,4};
