7c6dc33afd5ab08a19b52bd107e1902cc8fc6b0b
Sarthak-Malla <spm9513@nyu.edu>
2022-10-03 07:52:23 +0400

    1578: DP Working Code

--- 1578. Minimum Time to Make Rope Colorful/main.cpp
+++ 1578. Minimum Time to Make Rope Colorful/main.cpp
@@ -2,33 +2,21 @@
 #include<vector>
 using namespace std;
 
-string shift(string colors, vector<int>& neededTime, int index) {
-    for (int i = index; i < neededTime.size() - 1; i++) {
-        colors[i] = colors[i+1];
-        neededTime[i] = neededTime[i+1];
-    }
-    neededTime.pop_back();
-
-    return colors.substr(0, colors.size() - 1);
-}
 int minCost(string colors, vector<int>& neededTime) {
-    int time = 0;
-    for (int i = 0; i < colors.size() - 1; i++) {
-        if (colors[i+1] == colors[i]) {
-            if (neededTime[i+1] < neededTime[i]) {
-                time += neededTime[i+1];
-                colors = shift(colors, neededTime, i+1);
-            }
-            else {
-                time += neededTime[i];
-                colors = shift(colors, neededTime, i);
-            }
+    vector<int> time(neededTime.size());
+    time[0] = 0;
 
-            i--;
+    for (int i = 1; i < neededTime.size(); i++) {
+        if (colors[i] != colors[i-1])
+            time[i] = time[i-1];
+        else {
+            time[i] = time[i-1] + min(neededTime[i-1], neededTime[i]);
+            if (neededTime[i] < neededTime[i-1]) 
+                swap(neededTime[i], neededTime[i-1]);
         }
     }
 
-    return time;
+    return time[time.size() - 1];
 }
 
 int main(){
