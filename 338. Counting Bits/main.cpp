7481d5c0fd6c508e75ed2a922f59008d82bd63f1
Sarthak-Malla <spm9513@nyu.edu>
2022-10-03 12:32:30 +0400

    338: Working Code Using Even Odd Pattern

--- 338. Counting Bits/main.cpp
+++ 338. Counting Bits/main.cpp
@@ -2,20 +2,12 @@
 #include<vector>
 using namespace std;
 
-int hammingWeight(uint32_t n) {
-    int ans = 0;
-    while (n) {
-        ans += n & 1;
-        n = n >> 1;
-    }
-
-    return ans;
-}
 vector<int> countBits(int n) {
-    vector<int> ans(n+1, 0);
+    vector<int> ans(n + 1);
+    ans[0] = 0;
 
-    for (int i = 0; i < ans.size(); i++) {
-        ans[i] = hammingWeight(i);
+    for (int i = 1; i < n + 1; i++) {
+        ans[i] = ans[i/2] + (i%2);
     }
 
     return ans;
