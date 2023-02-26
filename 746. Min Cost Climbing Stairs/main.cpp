1475415fe069e9bc853259a0eca6a6730b8f8371
Sarthak-Malla <spm9513@nyu.edu>
2023-02-26 15:07:51 +0400

    746: Working Code [cp]

---
+++ 746. Min Cost Climbing Stairs/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int minCostClimbingStairs(vector<int>& cost) {
+	int n = cost.size();
+	vector<int> dp(n+1);
+	dp[0] = cost[0];
+	dp[1] = cost[1];
+
+	for (int i = 2; i < n; i++) {
+		dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
+	}
+
+	return min(dp[n-1], dp[n-2]);
+}
+
+int main(){
+	vector<int> cost = {10, 15, 20};
+	cout << minCostClimbingStairs(cost) << endl;
+
+	return 0;
+}
