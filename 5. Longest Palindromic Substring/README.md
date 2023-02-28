a0ee1278744abdf32cea0455ea9ce2d15c3ff534
Sarthak-Malla <spm9513@nyu.edu>
2023-02-28 23:52:44 +0400

    5: Working Code [cp]

--- 5. Longest Palindromic Substring/README.md
+++ 5. Longest Palindromic Substring/README.md
@@ -1,24 +1,21 @@
 # Longest Palindromic Substring
 
-<p>
-Given a string s, return the longest palindromic substring in s.
-</p>
-
-### Example 1:
-```
-Input: s = "babad"
-Output: "bab"
-Explanation: "aba" is also a valid answer.
-```
-
-### Example 2:
-```
-Input: s = "cbbd"
-Output: "bb"
-``` 
-
-### Constraints:
+Difficulty: Medium
+## Description
+<p>Given a string <code>s</code>, return <em>the longest</em> <span class="cursor-pointer relative text-dark-blue-s text-sm" data-keyword="palindromic-string"><div class="popover-wrapper inline-block" data-headlessui-state=""><div><div aria-expanded="false" data-headlessui-state="" id="headlessui-popover-button-:ra:"><em>palindromic</em></div></div></div></span> <span class="cursor-pointer relative text-dark-blue-s text-sm" data-keyword="substring-nonempty"><div class="popover-wrapper inline-block" data-headlessui-state=""><div><div aria-expanded="false" data-headlessui-state="" id="headlessui-popover-button-:rd:"><em>substring</em></div></div></div></span> in <code>s</code>.</p>
+<p> </p>
+<p><strong class="example">Example 1:</strong></p>
+<pre><strong>Input:</strong> s = "babad"
+<strong>Output:</strong> "bab"
+<strong>Explanation:</strong> "aba" is also a valid answer.
+</pre>
+<p><strong class="example">Example 2:</strong></p>
+<pre><strong>Input:</strong> s = "cbbd"
+<strong>Output:</strong> "bb"
+</pre>
+<p> </p>
+<p><strong>Constraints:</strong></p>
 <ul>
-<li><code>1 <= s.length <= 1000</code>
-<li><code>s consist of only digits and English letters.</code>
+<li><code>1 &lt;= s.length &lt;= 1000</code></li>
+<li><code>s</code> consist of only digits and English letters.</li>
 </ul>
--- 5. Longest Palindromic Substring/main.cpp
+++ 5. Longest Palindromic Substring/main.cpp
@@ -1,77 +1,41 @@
-#include<iostream>
-#include<string>
-#include<vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-bool isPalindrome(string s){
-    string reversed = "";
+string longestPalindrome(string s) {
+	int n = s.size();
+	int len = 0;
+	string res = "";
 
-    for (int i = 0; i < s.length(); i++){
-        reversed = s[i] + reversed;
-    }
+	for (int i = 0; i < n; i++) {
+		// odd
+		int l = i, r = i;
+		while (l >= 0 && r < n && s[l] == s[r]) {
+			if ((r - l + 1) > len) {
+				len = r-l+1;
+				res = s.substr(l, len);
+			}
+			l--;
+			r++;
+		}
 
-    return (s == reversed);
-}
+		// even
+		l = i, r = i + 1;
+		while (l >= 0 && r < n && s[l] == s[r]) {
+			if ((r - l + 1) > len) {
+				len = r-l+1;
+				res = s.substr(l, len);
+			}
+			l--;
+			r++;
+		}
+	}
 
-string longestPalindrome(string s){
-    int len = s.length();
-    vector<vector<int> > mat;
-    vector<int> inside;
-    for (int i = 0; i < len; i++){
-        for (int j = 0; j < len; j++){
-            inside.push_back(-1);
-        }
-
-        mat.push_back(inside);
-        inside.clear();
-    }
-
-    if (len == 1)
-        return s;
-    
-    string current = "";
-    string mid = "";
-    string longest = "";
-
-    for (int i = 0; i < len; i++){
-        for (int j = i; j < len; j++){
-            current = s.substr(i, j - i + 1);
-            if (mat[i][j] < 0){
-                if (j - i >= 2){
-                    if (mat[i+1][j-1] < 0){
-                        mat[i+1][j-1] = isPalindrome(current.substr(1, current.length() - 2));
-                    }
-
-                    if (mat[i+1][j-1] && s[i] == s[j]){
-                        mat[i][j] = 1;
-                    } else {
-                        mat[i][j] = 0;
-                    }
-
-                } else {
-                    mat[i][j] = (s[i] == s[j]);
-                }
-            }
-            if (mat[i][j] == 0)
-                continue;
-
-            if (mat[i][j] == 1){
-                if (current.length() > longest.length())
-                    longest = current;
-            }
-        }
-    }
-
-    return longest;
+	return res;
 }
 
 int main(){
+	string s = "babad";
+	cout << longestPalindrome(s) << endl;
 
-    string s = "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth";
-
-    string ans = longestPalindrome(s);   
-
-    cout<<ans<<endl; 
-
-    return 0;
+	return 0;
 }
