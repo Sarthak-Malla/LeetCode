ad5dfa71637e83c2d28016097a90744199247bca
Sarthak-Malla <spm9513@nyu.edu>
2023-06-12 15:23:55 +0400

    228: reformatted function

--- 228. Summary Ranges/main.cpp
+++ 228. Summary Ranges/main.cpp
@@ -2,37 +2,44 @@
 
 using namespace std;
 
-int main(){
-	vector<string> summaryRanges(vector<int>& nums) {
-        int n = nums.size();
-        vector<string> ans;
-        if (n < 1) return ans;
+vector<string> summaryRanges(vector<int>& nums) {
+    int n = nums.size();
+    vector<string> ans;
+    if (n < 1) return ans;
 
-        int prev = nums[0];
-        int first = prev;
-        int last = prev;
+    int prev = nums[0];
+    int first = prev;
+    int last = prev;
 
-        for (int i = 1; i < n; i++) {
-            if (nums[i] - 1 != prev) {
-                if (last == first) {
-                    ans.push_back(to_string(first));
-                } else {
-                    ans.push_back(to_string(first) + "->" + to_string(last));
-                }
-
-                first = nums[i];
+    for (int i = 1; i < n; i++) {
+        if (nums[i] - 1 != prev) {
+            if (last == first) {
+                ans.push_back(to_string(first));
+            } else {
+                ans.push_back(to_string(first) + "->" + to_string(last));
             }
-            last = nums[i];
-            prev = last;
-        }
 
-        if (last == first) {
-            ans.push_back(to_string(first));
-        } else {
-            ans.push_back(to_string(first) + "->" + to_string(last));
+            first = nums[i];
         }
+        last = nums[i];
+        prev = last;
+    }
 
-        return ans;
+    if (last == first) {
+        ans.push_back(to_string(first));
+    } else {
+        ans.push_back(to_string(first) + "->" + to_string(last));
+    }
+
+    return ans;
+}
+
+int main() {
+    vector<int> nums = {0, 1, 2, 4, 5, 7};
+    vector<string> ans = summaryRanges(nums);
+
+    for (string s : ans) {
+        cout << s << endl;
     }
 
 	return 0;
