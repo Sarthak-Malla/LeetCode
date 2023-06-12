495d33704451425511f727e12536948732cb817e
Sarthak-Malla <spm9513@nyu.edu>
2023-06-12 15:22:40 +0400

    1351: Working Code

--- 1351. Count Negative Numbers in a Sorted Matrix/main.cpp
+++ 1351. Count Negative Numbers in a Sorted Matrix/main.cpp
@@ -2,7 +2,29 @@
 
 using namespace std;
 
-int main(){
+int countNegatives(vector<vector<int>>& grid) {
+	int count = 0;
+	int m = grid.size();
+	int n = grid[0].size();
 
+	for (int i = 0; i < m; i++) {
+		for (int j = n - 1; j >= 0; j--) {
+			if (grid[i][j] < 0)
+				count++;
+			else
+				continue;
+		}
+	}
+
+	return count;
+}
+
+int main(){
+	vector<vector<int>> grid = {{4,3,2,-1},
+								{3,2,1,-1},
+								{1,1,-1,-2},
+								{-1,-1,-2,-3}};
+	cout << countNegatives(grid) << endl;
+	
 	return 0;
 }
