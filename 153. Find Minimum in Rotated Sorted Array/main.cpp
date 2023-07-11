1acafb15e9428a5d3244ec3ccd7e3374280da706
Sarthak-Malla <spm9513@nyu.edu>
2023-07-11 22:55:51 +0400

    binary search

--- 153. Find Minimum in Rotated Sorted Array/main.cpp
+++ 153. Find Minimum in Rotated Sorted Array/main.cpp
@@ -1,33 +1,28 @@
-#include<iostream>
-#include<vector>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-int binarySearch(vector<int>& nums, int left, int right) {
-    // base case
-    if (nums[left] < nums[right])
-        return nums[left];
-    
-    // getting the middle
-    int mid = (left + right)/2;
-
-    // this only happens when we are down to 2 elements
-    if (mid == left)
-        return min(nums[left], nums[right]); // the smaller one is the answer
-    
-    // condition of the minimum to be on the left of mid
-    if (nums[mid] < nums[right] || nums[mid] < nums[left]) 
-        return binarySearch(nums, left, mid);
-
-    return binarySearch(nums, mid, right);
+int binary(vector<int>& nums, int l, int r) {
+	if (l < r) {
+		int m = (l+r)/2;
+		if (nums[l] < nums[r])
+			return nums[l];
+		else {
+			if (nums[l] > nums[m])
+				return binary(nums, l, m);
+			else
+				return binary(nums, m+1, r);
+		}
+	}
+	return nums[l];
 }
-
 int findMin(vector<int>& nums) {
-    return binarySearch(nums, 0, nums.size() - 1);
+	return binary(nums, 0, nums.size()-1);
 }
 
 int main(){
-    vector<int> nums = {4,5,6,7,0,1,2};
-    cout << findMin(nums) << endl;
+	vector<int> nums = {4,5,6,7,0,1,2};
+	cout << findMin(nums) << endl;
 
-    return 0;
+	return 0;
 }
